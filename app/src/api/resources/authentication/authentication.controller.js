5b2c06d068b5df9e15dfd9c149100adcb5626c02
arbsutaj <sutajarb@gmail.com>
2020-12-18 21:52:33 +0100

    Modify exception handling in user service and auth controller

--- app/src/api/resources/authentication/authentication.controller.js
+++ app/src/api/resources/authentication/authentication.controller.js
@@ -1,26 +1,33 @@
 import userService from "../user/user.service";
 import authenticationService from "./authentication.service";
-import {OkResponse, returnInternalExceptionResponse} from "../../helpers/utils";
+import {
+    exceptionResponse,
+    internalExceptionResponse,
+    okResponse,
+    validationExceptionResponse
+} from "../../helpers/utils";
 
 export default {
     async login(req, res) {
         try {
             const {user, correctCredentialsError, validationError} = await userService.validateLoginRequest(req.body);
-            if (validationError)
-                return res.status(400).json({status: '400', error: validationError});
-            if (correctCredentialsError)
-                return res.status(401).json({status: '401', error: correctCredentialsError});
+            if (validationError || correctCredentialsError) {
+                if (validationError)
+                    return validationExceptionResponse(res, validationError);
+
+                return exceptionResponse(res, correctCredentialsError);
+            }
 
             const {tokenPayload} = authenticationService.generateToken(user._id);
 
-            return OkResponse(res, tokenPayload);
+            return okResponse(res, tokenPayload);
         } catch (err) {
-            returnInternalExceptionResponse(res);
+            return internalExceptionResponse(res);
         }
     },
     async getLoggedInUser(req, res) {
         const {userDto} = await userService.toDto(req.user);
 
-        return OkResponse(res, userDto);
+        return okResponse(res, userDto);
     }
 }
